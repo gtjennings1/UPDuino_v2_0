6b1576f85f6379ac03ff3ca359c6df1af33622e1
gtjennings1 <32719401+gtjennings1@users.noreply.github.com>
2018-01-31 17:19:20 -0600

    Add files via upload

---
+++ RGB_LED_BLINK_20170606/rgb/synlog.tcl
@@ -0,0 +1,35 @@
+history clear
+project -load rgb_syn.prj
+text_select 77 9 77 48
+text_select 73 1 80 62
+text_select 72 1 80 62
+text_select 71 12 71 18
+text_select 127 1 153 3
+text_select 125 1 140 3
+text_select 105 1 112 23
+text_select 90 1 92 1
+text_select 80 26 80 31
+text_select 83 5 83 69
+text_select 80 1 84 79
+text_select 80 26 80 33
+text_select 92 1 92 37
+text_select 76 1 76 5
+text_select 70 8 70 23
+text_select 106 1 106 19
+text_select 96 1 119 1
+text_select 105 26 105 53
+text_select 101 63 101 74
+text_select 102 64 102 75
+text_select 103 64 103 75
+text_select 87 1 91 79
+text_select 102 4 102 29
+text_select 98 26 98 33
+text_select 101 1 103 33
+text_select 1 1 63 1
+text_select 3 28 3 34
+text_select 3 35 3 46
+text_select 11 8 11 10
+text_select 11 1 11 9
+text_select 48 1 49 1
+project -run  
+project -close C:/projects/GPU/UP_MiniGPU/FPGA/UPDuino_RGB/rgb/rgb_syn.prj
